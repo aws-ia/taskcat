1ef3d9edf28d3ad5f8f0dbdc46928a48fd4109f2
Tony Vattathil <avattathil@gmail.com>
2016-06-26 12:04:06 -0700

    Initial project stub

---
+++ taskcat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# authors:avattathil@gmail.com
+# upstream_repo: https://avattathil/taskcat.io
+# docs: http://taskcat.io
+#
+# taskcat is short for task (cloudformation automated testing)
+# This program takes as input:
+# cfn template and json formated parameter input file
+# inputs can be passed as cli for single test
+# for more diverse scenarios you can use a ymal configuration 
+# Planed Features: 
+# - Tests in only specific regions
+# - Email test results to owner of project
+# Version .01
