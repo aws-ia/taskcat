c0f85c7ae5ec95919238718dc31b05c291dd2d63
Anthony Vattathil <avattathil@gmail.com>
2016-11-22 17:36:48 -0800

    Added docs publishing to buildtools

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This script requrest setuptools and mkdocs
 ./build-tools/newversion.sh ./setup.py
 ./build-tools/newversion.sh ./taskcat/taskcat.py
 VERSION=$(grep version taskcat/taskcat.py |head -1 | awk -F'=|,' '{print $2}' | sed -e s/\'//|tr -d " "|tr -d "'")
@@ -6,6 +7,8 @@ echo $VERSION
 rm  dist/*
 python setup.py sdist 
 python setup.py bdist_wheel 
+python -m mkdocs gh-deploy --clean  
+python setup.py upload_docs --upload-dir=site
 
 git tag $VERSION
 git push --tags origin master
