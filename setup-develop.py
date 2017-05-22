0d8a3d2cf816b22ad06ee38eec9383600a2ab7db
Tony Vattathil <avattathil@gmail.com>
2017-05-22 11:31:49 -0700

    Added setup for develop branch

--- setup-develop.py
+++ setup-develop.py
@@ -1,4 +1,26 @@
 from setuptools import setup
+import re
+import requests
+
+master_version_file = 'taskcat/master_version'
+develop_version_file = 'taskcat/develop_version'
+
+def get_pip_version(pkginfo_url):
+    pkginfo = requests.get(pkginfo_url).text
+    for record in pkginfo.split('\n'):
+        if record.startswith('Version'):
+            current_version = str(record).split(':', 1)
+            return (current_version[1]).strip()
+
+
+current_develop_version = get_pip_version('https://testpypi.python.org/pypi?name=taskcat&:action=display_pkginfo')
+print("PyPi Develop Version is [{}]".format(current_develop_version))
+
+development_version =re.sub('\d$', lambda version: str(int(version.group(0)) + 1), current_develop_version)
+
+print("[new] PyPi Develop Version is [{}]".format(development_version))
+
+current_develop_version = get_pip_version('https://testpypi.python.org/pypi?name=taskcat&:action=display_pkginfo')
 
 setup(
     name='taskcat',
@@ -7,7 +29,7 @@ setup(
     author='Tony Vattathil, Santiago Cardenas, Shivansh Singh',
     author_email='tonynv@amazon.com, sancard@amazon.com, sshvans@amazon.com',
     url='https://aws-quickstart.github.io/taskcat/',
-    version='0.1.70.dev2',
+    version=development_version,
     license='Apache License 2.0',
     download_url='https://github.com/aws-quickstart/taskcat/tarball/master',
     classifiers=[
