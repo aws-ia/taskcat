e41560cf3d0dff776c9221173286aa21537d9307
Tony Vattathil <avattathil@gmail.com>
2017-05-26 16:31:29 -0700

    Added dynamic versioning to pip setup file

--- setup-develop.py
+++ setup-develop.py
@@ -1,4 +1,17 @@
 from setuptools import setup
+import re
+import requests
+def get_pip_version(pkginfo_url):
+    pkginfo = requests.get(pkginfo_url).text
+    for record in pkginfo.split('\n'):
+        if  record.startswith('Version'):
+            current_version = str(record).split(':',1)
+            return (current_version[1]).strip()
+
+
+current_develop_version = get_pip_version('https://testpypi.python.org/pypi?name=taskcat&:action=display_pkginfo')
+development_version =re.sub('\d$', lambda x: str(int(x.group(0)) + 1), current_develop_version)
+
 
 setup(
     name='taskcat',
@@ -7,7 +20,7 @@ setup(
     author='Tony Vattathil, Santiago Cardenas, Shivansh Singh',
     author_email='tonynv@amazon.com, sancard@amazon.com, sshvans@amazon.com',
     url='https://aws-quickstart.github.io/taskcat/',
-    version='0.1.76.dev0',
+    version=development_version,
     license='Apache License 2.0',
     download_url='https://github.com/aws-quickstart/taskcat/tarball/master',
     classifiers=[
@@ -22,6 +35,6 @@ setup(
 
     keywords=['aws', 'cloudformation', 'cloud', 'cloudformation testing', 'cloudformation deploy', 'taskcat'],
 
-    install_requires=['boto3', 'pyfiglet', 'pyyaml', 'tabulate', 'yattag', 'pkg_resources'],
+    install_requires=['boto3', 'pyfiglet', 'pyyaml', 'tabulate', 'yattag', 'pkg_resources']
 
 )
