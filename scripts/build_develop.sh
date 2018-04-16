16b9dbee3e9209c8c7913b139da321ed43c40756
Tony Vattathil <avattathil@gmail.com>
2018-04-16 16:03:02 -0700

    Remove push to test pypi

--- scripts/build_develop.sh
+++ scripts/build_develop.sh
@@ -16,5 +16,3 @@ cp alchemist.py bin/alchemist
 cp scripts/setup_stub-develop.py ./setup_stub-develop.py
 # Update Pip Version
 python3 -c 'import datetime; print (open("./setup_stub-develop.py").read().replace("VERSION_STUB",datetime.datetime.now().strftime("%Y.%m%d.%H%M%S")+".dev0"))' >setup.py
-# Push to test pypi
-python3 setup.py sdist && twine-3 upload dist/* -r test
