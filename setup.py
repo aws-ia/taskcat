14cc48139f2576890994855f442e87fae0bebb97
Tony Vattathil <avattathil@gmail.com>
2018-09-18 14:09:28 -0700

    Versioning update: - Removes taskcat/.version file - Update setup.py (move static versions) - Implement bump versioning - Aggregates changes release branch (when tests are successful)

--- setup.py
+++ setup.py
@@ -3,19 +3,8 @@ import datetime
 import unittest
 import os
 
-def get_version():
-    _version_file = 'taskcat/.version'
-
-    if os.path.exists(_version_file) and os.path.getsize(_version_file) > 0:
-        _version = open(_version_file, 'r').read()
-    else:
-        _version = datetime.datetime.now().strftime("%Y.%-m%-d.%-H%-M")
-        print("Creating new version {}".format(_version))
-        update_version = open(_version_file, "w")
-        update_version.write(''.join(_version.splitlines()))
-        update_version.close()
-
-    return _version
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 def test_suite():
     test_loader = unittest.TestLoader()
@@ -29,9 +18,9 @@ setup(
     author='Tony Vattathil, Santiago Cardenas, Shivansh Singh, Jay McConnell, Andrew Glenn' ,
     author_email='tonynv@amazon.com, sancard@amazon.com, sshvans@amazon.com, jmmccon@amazon.com, andglenn@amazon.com',
     url='https://aws-quickstart.github.io/taskcat/',
-    version=get_version(),
     license='Apache License 2.0',
     download_url='https://github.com/aws-quickstart/taskcat/tarball/master',
+    version = '0.5.0',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -47,7 +36,7 @@ setup(
         'bin/alchemist'
     ],
     keywords=['aws', 'cloudformation', 'cloud', 'cloudformation testing', 'cloudformation deploy', 'taskcat'],
-    install_requires=['boto3', 'pyfiglet', 'pyyaml', 'tabulate', 'yattag', 'cfn-lint'],
+    install_requires=required,
     test_suite='setup.test_suite'
 
 )
