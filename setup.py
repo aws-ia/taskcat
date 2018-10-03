02fa3fe6cf834f2d8876309479599e8b87e7fefa
Tony Vattathil <avattathil@gmail.com>
2018-10-03 02:15:26 -0700

    Add requirements.txt to package MANIFEST.in Fixes: https://github.com/aws-quickstart/taskcat/issues/143 - new-version 0.6.0 → Added to [0.7.1] [skip ci]

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url='https://aws-quickstart.github.io/taskcat/',
     license='Apache License 2.0',
     download_url='https://github.com/aws-quickstart/taskcat/tarball/master',
-    version = '0.6.1',
+    version = '0.7.1',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -37,7 +37,8 @@ setup(
     ],
     keywords=['aws', 'cloudformation', 'cloud', 'cloudformation testing', 'cloudformation deploy', 'taskcat'],
     install_requires=required,
-    test_suite='setup.test_suite'
+    test_suite='setup.test_suite',
+    include_package_data=True
 
 )
 
