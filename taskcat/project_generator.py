5869fd9f2859b1ffb20116069b7c86889416c31e
Ruben Duque <duqruben@amazon.com>
2018-11-27 09:43:34 -0800

    address cr feedback
    - renamed a badly named `path` variable that could collide with a globally
      imported `os.path`
    
    - removed unused argument in method.
    
    - no longer setting the loglevel
    
    - use non deprecated warning method.
    
    - fix template typo.

--- taskcat/project_generator.py
+++ taskcat/project_generator.py
@@ -16,7 +16,8 @@ def template_paths(template_dir, templates):
     return [
         template_dir + os.sep + t
         for t in templates
-        if t.endswith(TEMPLATE_FILE_EXTENSION)]
+        if t.endswith(TEMPLATE_FILE_EXTENSION)
+    ]
 
 
 ProjectConfiguration = namedtuple(
@@ -25,7 +26,6 @@ ProjectConfiguration = namedtuple(
 )
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 
 class ProjectGenerator:
@@ -43,26 +43,26 @@ class ProjectGenerator:
             self._make_project_directory(project_path)
 
             template_filepaths = template_paths(directory, files)
-            self._generate_project_files(template_filepaths, project_path)
+            self._generate_project_files(template_filepaths)
 
     def _traverse_templates(self):
         return self.filesystem.traverse_templates(self.config.project_type)
 
-    def _full_destination_path(self, path):
+    def _full_destination_path(self, destination_path):
         templates_root = self.filesystem.project_templates_root(
             self.config.project_type
         )
-        path = path.replace(templates_root, '')
-        return full_path(self.destination, path)
+        destination_path = destination_path.replace(templates_root, '')
+        return full_path(self.destination, destination_path)
 
     def _make_project_directory(self, project_directory):
         try:
             logger.info('creating {}'.format(project_directory))
             self.filesystem.create_project_directory(project_directory)
         except FileExistsError as e:
-            logging.warn("{} - skipping...".format(e))
+            logging.warning("{} - skipping...".format(e))
 
-    def _generate_project_files(self, template_filepaths, project_directory):
+    def _generate_project_files(self, template_filepaths):
         logger.info('generating files...')
         for filepath in template_filepaths:
             template = self.filesystem.load_template(filepath)
