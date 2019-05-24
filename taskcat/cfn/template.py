0960512d03ff4a7112f52e2f45a5106b09d9119b
Jay McConnell <jmmccon@amazon.com>
2019-05-23 18:20:28 -0700

    return early in the exception case

--- taskcat/cfn/template.py
+++ taskcat/cfn/template.py
@@ -98,13 +98,13 @@ class Template:
         return ''
 
     def _get_relative_url(self, path: str) -> str:
-        url = ''
-        if self.url:
-            suffix = str(self.template_path).replace(str(self.project_root), '')
-            suffix_length = len(suffix.lstrip('/').split("/"))
-            url_prefix = '/'.join(self.url.split('/')[0:-suffix_length])
-            suffix = str(path).replace(str(self.project_root), '')
-            url = url_prefix + suffix
+        if not self.url:
+            return ''
+        suffix = str(self.template_path).replace(str(self.project_root), '')
+        suffix_length = len(suffix.lstrip('/').split("/"))
+        url_prefix = '/'.join(self.url.split('/')[0:-suffix_length])
+        suffix = str(path).replace(str(self.project_root), '')
+        url = url_prefix + suffix
         return url
 
     def url_prefix(self) -> str:
