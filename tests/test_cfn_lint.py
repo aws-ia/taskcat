5742fd269c0e3e399a77676595b7326d737b5ee8
Jay McConnell <jmmccon@amazon.com>
2019-07-03 12:31:18 -0700

    update tests

--- tests/test_cfn_lint.py
+++ tests/test_cfn_lint.py
@@ -10,56 +10,52 @@ from io import StringIO
 import yaml
 
 from taskcat.cfn_lint import Lint
+from taskcat.config import Config
 
 test_cases = [
     {
         "config": {
-            "global": {
-                "qsname": "test-config",
-                "regions": ["eu-west-1"]
-            },
-            "tests": {
-                "test1": {}
+            "global": {"qsname": "test-config", "regions": ["eu-west-1"]},
+            "tests": {"test1": {}},
+        },
+        "templates": {"test1": """{"Resources": {}}"""},
+        "expected_lints": {
+            "test1": {
+                "regions": ["eu-west-1"],
+                "template_file": "templates/taskcat_test_template_test1",
+                "results": {"templates/taskcat_test_template_test1": []},
             }
         },
-        "templates": {
-            "test1": """{"Resources": {}}"""
-        },
-        "expected_lints": {'test1': {'regions': ['eu-west-1'], 'template_file': 'templates/taskcat_test_template_test1',
-                                     'results': {'templates/taskcat_test_template_test1': []}}},
-        "expected_output": "\x1b[0;30;43m[INFO   ]\x1b[0m :Lint passed for test test1 on template templates/taskcat_test_template_test1:\n"
+        "expected_output": "\x1b[0;30;43m[INFO   ]\x1b[0m :Lint passed for test test1 on template templates/taskcat_test_template_test1:\n",
     },
     {
         "config": {
-            "global": {
-                "qsname": "test-config2",
-                "regions": ["eu-west-1"]
-            },
-            "tests": {
-                "test1": {}
-            }
+            "global": {"qsname": "test-config2", "regions": ["eu-west-1"]},
+            "tests": {"test1": {}},
         },
         "templates": {
             "test1": """{"Resources": {"Name": {"Type":"AWS::Not::Exist"}}}"""
         },
-        "expected_lints": {'test1': {'regions': ['eu-west-1'],
-           'results': {'templates/taskcat_test_template_test1': [
-               '[E3001: Basic CloudFormation Resource Check] (Invalid or unsupported Type AWS::Not::Exist for resource Name in eu-west-1) matched templates/taskcat_test_template_test1:1']},
-           'template_file': 'templates/taskcat_test_template_test1'}},
+        "expected_lints": {
+            "test1": {
+                "regions": ["eu-west-1"],
+                "results": {
+                    "templates/taskcat_test_template_test1": [
+                        "[E3001: Basic CloudFormation Resource Check] (Invalid or unsupported Type AWS::Not::Exist for resource Name in eu-west-1) matched templates/taskcat_test_template_test1:1"
+                    ]
+                },
+                "template_file": "templates/taskcat_test_template_test1",
+            }
+        },
         "expected_output": """[0;30;41m[ERROR  ][0m :Lint detected issues for test test1 on template templates/taskcat_test_template_test1:
 [0;30;41m[ERROR  ][0m :    line 1 [E3001] [Basic CloudFormation Resource Check] (Invalid or unsupported Type AWS::Not::Exist for resource Name in eu-
                       west-1)
-"""
+""",
     },
     {
         "config": {
-            "global": {
-                "qsname": "test-config3",
-                "regions": ["eu-west-1"]
-            },
-            "tests": {
-                "test1": {}
-            }
+            "global": {"qsname": "test-config3", "regions": ["eu-west-1"]},
+            "tests": {"test1": {}},
         },
         "templates": {
             "test1": r"""{
@@ -70,29 +66,34 @@ test_cases = [
     }
 }"""
         },
-        "expected_lints": {'test1': {'regions': ['eu-west-1'], 'template_file': 'templates/taskcat_test_template_test1',
-                                     'results': {}}},
+        "expected_lints": {
+            "test1": {
+                "regions": ["eu-west-1"],
+                "template_file": "templates/taskcat_test_template_test1",
+                "results": {},
+            }
+        },
         "expected_output": "",
-        "expected_error_output": '[0;30;41m[ERROR  ][0m :Linter failed to load template templates/taskcat_test_template_test1 "found unknown escape character" line 3, column 31\n'
+        "expected_error_output": '[0;30;41m[ERROR  ][0m :Linter failed to load template templates/taskcat_test_template_test1 "found unknown escape character" line 3, column 31\n',
     },
     {
         "config": {
-            "global": {
-                "qsname": "test-config4",
-                "regions": ["eu-west-1"]
-            },
-            "tests": {
-                "test1": {}
-            }
+            "global": {"qsname": "test-config4", "regions": ["eu-west-1"]},
+            "tests": {"test1": {}},
         },
         "templates": {
             "test1": """{"Resources": {"Name": {"Type":"AWS::CloudFormation::Stack","Properties":{"TemplateURL": "broken"}}}}"""
         },
-        "expected_lints": {'test1': {'regions': ['eu-west-1'], 'template_file': 'templates/taskcat_test_template_test1',
-                                     'results': {'templates/taskcat_test_template_test1': []}}},
+        "expected_lints": {
+            "test1": {
+                "regions": ["eu-west-1"],
+                "template_file": "templates/taskcat_test_template_test1",
+                "results": {"templates/taskcat_test_template_test1": []},
+            }
+        },
         "expected_output": "[0;30;43m[INFO   ][0m :Lint passed for test test1 on template templates/taskcat_test_template_test1:\n",
-        "expected_error_output": """[0;30;41m[ERROR  ][0m :Linter failed to load template broken "[Errno 2] No such file or directory: 'broken'"\n"""
-    }
+        "expected_error_output": """[0;30;41m[ERROR  ][0m :Linter failed to load template broken "[Errno 2] No such file or directory: 'broken'"\n""",
+    },
 ]
 
 
@@ -115,12 +116,13 @@ def flatten_rule(lints):
     for test in lints.keys():
         for result in lints[test]["results"].keys():
             for i in range(0, len(lints[test]["results"][result])):
-                lints[test]["results"][result][i] = str(lints[test]["results"][result][i])
+                lints[test]["results"][result][i] = str(
+                    lints[test]["results"][result][i]
+                )
     return lints
 
 
 class TestCfnLint(unittest.TestCase):
-
     def test_lint(self):
         base_path = "/tmp/lint_test/"
         mkdir(base_path)
@@ -128,22 +130,22 @@ class TestCfnLint(unittest.TestCase):
             for test_case in test_cases:
                 qs_path = base_path + test_case["config"]["global"]["qsname"] + "/"
                 mkdir(qs_path)
-                mkdir(qs_path + 'ci')
-                mkdir(qs_path + 'templates')
+                mkdir(qs_path + "ci")
+                mkdir(qs_path + "templates")
                 os.chdir(qs_path)
                 template_path = "taskcat_test_template"
                 config_path = "./ci/taskcat_test_config"
-                for test in test_case['config']["tests"].keys():
+                for test in test_case["config"]["tests"].keys():
                     template_file = template_path + "_" + test
-                    test_case['config']["tests"][test]["template_file"] = template_file
-                    with open("./templates/" + template_file, 'w') as f:
+                    test_case["config"]["tests"][test]["template_file"] = template_file
+                    with open("./templates/" + template_file, "w") as f:
                         f.write(test_case["templates"][test])
-                with open(config_path, 'w') as f:
-                    f.write(yaml.safe_dump(test_case['config']))
+                with open(config_path, "w") as f:
+                    f.write(yaml.safe_dump(test_case["config"]))
                 with captured_output() as (out, err):
-                    lint = Lint(config=config_path)
+                    config = Config(project_config_path=config_path, project_root="../")
+                    lint = Lint(config=config)
                 lint.lints = flatten_rule(lint.lints)
-                self.assertEqual(test_case['expected_lints'], lint.lints)
+                self.assertEqual(test_case["expected_lints"], lint.lints)
         finally:
             shutil.rmtree(base_path)
-
