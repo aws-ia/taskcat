82edd8ff83c793f57fc0acc973baa6c33cc53cdb
Jay McConnell <jmmccon@amazon.com>
2019-07-10 10:56:38 -0700

    removing redundant file headers, contributors can be easily identified from github ui

--- taskcat/_client_factory.py
+++ taskcat/_client_factory.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# authors:
-# Tony Vattathil <tonynv@amazon.com>, <avattathil@gmail.com>
-# Santiago Cardenas <sancard@amazon.com>, <santiago[dot]cardenas[at]outlook[dot]com>
-# Shivansh Singh <sshvans@amazon.com>,
-# Jay McConnell <jmmccon@amazon.com>,
-# Andrew Glenn <andglenn@amazon.com>
 import logging
 from threading import Lock
 from time import sleep
--- taskcat/_s3_sync.py
+++ taskcat/_s3_sync.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# authors:
-# Tony Vattathil <tonynv@amazon.com>, <avattathil@gmail.com>
-# Santiago Cardenas <sancard@amazon.com>, <santiago[dot]cardenas[at]outlook[dot]com>
-# Shivansh Singh <sshvans@amazon.com>,
-# Jay McConnell <jmmccon@amazon.com>,
-# Andrew Glenn <andglenn@amazon.com>
 from __future__ import print_function
 
 import fnmatch
