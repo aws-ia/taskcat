7155a31d09837980e5af980d42fa1f29c470aa79
Tony Vattathil <avattathil@gmail.com>
2019-08-28 21:03:43 -0700

    Wire up linting

--- taskcat/_cli_modules/test.py
+++ taskcat/_cli_modules/test.py
@@ -1,5 +1,7 @@
 # flake8: noqa B950,F841
+from taskcat._cfn_lint import Lint as TaskCatLint
 from taskcat._config import Config
+from taskcat.exceptions import TaskCatException
 
 
 class Test:
@@ -15,10 +17,17 @@ class Test:
         :param project_root: root path of the project relative to input_file
         """
         config = Config(
-            input_file, project_root=project_root
-        )  # pylint: disable=unused-variable
+            project_root=project_root,
+            # TODO detect if input file is taskcat config or CloudFormation template
+            project_config_path=input_file,
+        )
         # 1. build lambdas
         # 2. lint
+        lint = TaskCatLint(config, strict=False)
+        errors = lint.lints[1]
+        lint.output_results()
+        if errors or not lint.passed:
+            raise TaskCatException("Lint failed with errors")
         # 3. s3 sync
         # 4. validate
         # 5. launch stacks
