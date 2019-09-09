cabbf15f31bf86db637b1c1f9e89f0d29c36a12d
Jay McConnell <jmmccon@amazon.com>
2019-09-09 12:53:25 -0700

    make input file optional default to default config file name in current working directory

--- taskcat/_cli_modules/lint.py
+++ taskcat/_cli_modules/lint.py
@@ -10,7 +10,12 @@ LOG = logging.getLogger(__name__)
 class Lint:
     """checks CloudFormation templates for issues using cfn-python-lint"""
 
-    def __init__(self, input_file: str, project_root: str = "./", strict: bool = False):
+    def __init__(
+        self,
+        input_file: str = "./.taskcat.yml",
+        project_root: str = "./",
+        strict: bool = False,
+    ):
         """
         :param input_file: path to project config or CloudFormation template
         :param project_root: base path for project
