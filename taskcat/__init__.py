1a75cfa41744369a8aecd396b9c7aa6a638a4448
Tony Vattathil <avattathil@gmail.com>
2019-10-30 16:25:06 -0700

    Add __all__ to init.py

--- taskcat/__init__.py
+++ taskcat/__init__.py
@@ -5,3 +5,5 @@ from ._cfn.stack import Stack  # noqa: F401
 from ._cfn.template import Template  # noqa: F401
 from ._cli import main  # noqa: F401
 from ._config import Config  # noqa: F401
+
+__all__ = ["Stack", "Template", "Config", "main"]
