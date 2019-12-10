e290c65469c879652d6c5f409a1e5da0d3f97d3b
Jay McConnell <jmmccon@amazon.com>
2019-12-10 11:06:25 -0800

    remove duplicated code fragment

--- taskcat/_cfn/template.py
+++ taskcat/_cfn/template.py
@@ -82,13 +82,8 @@ class Template:
         return ""
 
     def _get_relative_url(self, path: str) -> str:
-        if not self.url:
-            return ""
-        suffix = str(self.template_path).replace(str(self.project_root), "")
-        suffix_length = len(suffix.lstrip("/").split("/"))
-        url_prefix = "/".join(self.url.split("/")[0:-suffix_length])
         suffix = str(path).replace(str(self.project_root), "")
-        url = url_prefix + suffix
+        url = self.url_prefix() + suffix
         return url
 
     def url_prefix(self) -> str:
