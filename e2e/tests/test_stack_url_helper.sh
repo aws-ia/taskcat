421684c80154bf1e0732c7904ed41c343aeb8678
Andrew Gargan <gargana@amazon.com>
2020-03-03 23:26:22 +0000

    Modified test for stackurlhelper

--- e2e/tests/test_stack_url_helper.sh
+++ e2e/tests/test_stack_url_helper.sh
@@ -3,10 +3,10 @@
 EXIT_CODE=0
 PROJECT_ROOT='../../tests/data/stackurlhelper/badtemplateurl'
 
-PYTHONIOENCODING=UTF-8 ${BIN} -d lint -p ${PROJECT_ROOT}  >& /tmp/output || EXIT_CODE=$?
+PYTHONIOENCODING=UTF-8 ${BIN} -d test run -p ${PROJECT_ROOT}  >& /tmp/output || EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} -ne 0 ]] ; then
-    echo '$ taskcat lint -p ./tests/data/stackurlhelper/badtemplateurl'
+    echo '$ taskcat test run -p ./tests/data/stackurlhelper/badtemplateurl'
     cat /tmp/output
     echo "FAILED: expected exit code to be 0"
     exit 1
