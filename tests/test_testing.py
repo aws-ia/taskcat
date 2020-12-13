cb3b0d5f0a3e229736bca94bff342af1121b79ae
Levi Blaney <shadycuz@gmail.com>
2020-12-13 03:52:15 -0500

    added additional tests for TestManager

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -5,14 +5,19 @@ from pathlib import Path
 
 import yaml
 
-import mock
+from mock import ANY, MagicMock, Mock, patch
 from taskcat._tui import TerminalPrinter
+from taskcat.exceptions import TaskCatException
 from taskcat.testing import TestManager
+
+# Save some typing
+m = Mock
+mm = MagicMock
 
 
 class TestTestManager(unittest.TestCase):
-    @mock.patch("taskcat.testing.manager.Config", autospec=True)
-    def test_from_file(self, mock_config):
+    @patch("taskcat.testing.manager.Config", autospec=True)
+    def test_from_file(self, mock_config: mm):
         base_path = "./" if os.getcwd().endswith("/tests") else "./tests/"
         base_path = Path(base_path + "data/nested-fail").resolve()
         input_path = base_path / ".taskcat.yml"
@@ -47,8 +52,8 @@ class TestTestManager(unittest.TestCase):
             "Should turn our str into a Path.",
         )
 
-    @mock.patch("taskcat.testing.manager.Config", autospec=True)
-    def test_from_dict(self, mock_config):
+    @patch("taskcat.testing.manager.Config", autospec=True)
+    def test_from_dict(self, mock_config: mm):
         base_path = "./" if os.getcwd().endswith("/tests") else "./tests/"
         base_path = Path(base_path + "data/nested-fail").resolve()
         input_path = base_path / ".taskcat.yml"
@@ -81,8 +86,8 @@ class TestTestManager(unittest.TestCase):
             self.assertTrue("config" in item, "Should contain config key.")
             self.assertTrue("source" in item, "Should contain source key.")
 
-    @mock.patch("taskcat.testing.manager.Config", autospec=True)
-    def test_default(self, mock_config):
+    @patch("taskcat.testing.manager.Config", autospec=True)
+    def test_default(self, mock_config: mm):
 
         test_manager = TestManager(mock_config)
 
@@ -98,30 +103,30 @@ class TestTestManager(unittest.TestCase):
             "Should create a TerminalPrinter if none passed in.",
         )
 
-    @mock.patch("taskcat.testing.manager.Config", autospec=True)
-    def test_printer_overide(self, mock_config):
+    @patch("taskcat.testing.manager.Config", autospec=True)
+    def test_printer_overide(self, mock_config: mm):
 
-        mock_printer = mock.MagicMock()
+        mock_printer = MagicMock()
 
         test_manager = TestManager(mock_config, mock_printer)
 
         self.assertEqual(test_manager.printer, mock_printer, "Should use our printer.")
 
-    @mock.patch("taskcat.testing.manager.Stacker", autospec=True)
-    @mock.patch("taskcat.testing.manager.stage_in_s3", autospec=True)
-    @mock.patch("taskcat.testing.manager.LambdaBuild", autospec=True)
-    def test_start_default(self, mock_lambda, mock_stage_s3, mock_stacker):
+    @patch("taskcat.testing.manager.Stacker", autospec=True)
+    @patch("taskcat.testing.manager.stage_in_s3", autospec=True)
+    @patch("taskcat.testing.manager.LambdaBuild", autospec=True)
+    def test_start_default(self, mock_lambda: mm, mock_stage_s3: mm, mock_stacker: mm):
         base_path = "./" if os.getcwd().endswith("/tests") else "./tests/"
         base_path = Path(base_path + "data/nested-fail").resolve()
 
         test_manager = TestManager.from_file(str(base_path))
 
         # Create all the config mocks
-        mock_get_buckets = mock.Mock()
-        mock_get_regions = mock.Mock()
-        mock_get_parameters = mock.Mock()
-        mock_get_tests = mock.Mock()
-        mock_printer = mock.Mock()
+        mock_get_buckets: m = Mock()
+        mock_get_regions: m = Mock()
+        mock_get_parameters: m = Mock()
+        mock_get_tests: m = Mock()
+        mock_printer: m = Mock()
 
         # Assign all the config mocks
         test_manager.config.get_buckets = mock_get_buckets
@@ -142,10 +147,10 @@ class TestTestManager(unittest.TestCase):
         )
         mock_get_regions.assert_called_once()
         mock_get_parameters.assert_called_once_with(
-            mock_get_buckets.return_value, mock_get_regions.return_value, mock.ANY
+            mock_get_buckets.return_value, mock_get_regions.return_value, ANY
         )
         mock_get_tests.assert_called_once_with(
-            mock.ANY,
+            ANY,
             mock_get_regions.return_value,
             mock_get_buckets.return_value,
             mock_get_parameters.return_value,
@@ -160,23 +165,128 @@ class TestTestManager(unittest.TestCase):
             stacker=test_manager.test_definition
         )
 
-    def test_start_skip_upload(self):
-        pass
+    @patch("taskcat.testing.manager.Stacker", autospec=True)
+    @patch("taskcat.testing.manager.stage_in_s3", autospec=True)
+    @patch("taskcat.testing.manager.Config")
+    def test_start_skip_upload(
+        self, mock_config: mm, mock_stage_s3: mm, mock_stacker: mm
+    ):
 
-    def test_start_list_disable(self):
-        pass
+        test_manager = TestManager(mock_config())
 
-    def test_end_default(self):
-        pass
+        config_obj = mock_config.return_value
 
-    def test_end_no_delete(self):
-        pass
+        config_obj.config.project.s3_bucket = ""
 
-    def test_end_keep_failed(self):
-        pass
+        # Should fail if we don't specify an existing s3 bucket
+        with self.assertRaises(TaskCatException) as ex:
+            test_manager.start(skip_upload=True)
 
-    def test_report(self):
-        pass
+        self.assertEqual(
+            str(ex.exception),
+            "cannot skip_buckets without specifying s3_bucket in config",
+        )
+
+        test_manager.config.config.project.s3_bucket = "FakeBucket"
+
+        test_manager.start(skip_upload=True)
+
+        # Test all the mocks
+        self.assertFalse(mock_stage_s3.called, "Should not stage in s3.")
+
+    @patch("taskcat.testing.manager.Stacker", autospec=True)
+    @patch("taskcat.testing.manager.stage_in_s3", autospec=True)
+    @patch("taskcat.testing.manager.Config")
+    @patch("taskcat.testing.manager.TaskCatLint", autospec=True)
+    def test_start_lint(
+        self, mock_lint: mm, mock_config: mm, mock_stage_s3: mm, mock_stacker: mm
+    ):
+
+        test_manager = TestManager(mock_config())
+
+        config_obj = mock_config.return_value
+        config_obj.config.project.s3_bucket = ""
+        config_obj.config.project.package_lambda = False
+        lint_obj = mock_lint.return_value
+        lint_obj.lints = [False, False]
+        lint_obj.passed = True
+
+        test_manager.start(lint_disable=True)
+
+        lint_obj.output_results.assert_not_called()
+
+        test_manager.start()
+
+        lint_obj.output_results.assert_called_once()
+
+        lint_obj.passed = False
+
+        # Should fail if we don't specify an existing s3 bucket
+        with self.assertRaises(TaskCatException) as ex:
+            test_manager.start()
+
+        self.assertEqual(str(ex.exception), "Lint failed with errors")
+
+    @patch("taskcat.testing.manager.Config")
+    def test_end_default(self, mock_config: mm):
+        test_manager = TestManager(mock_config())
+
+        td_mock = MagicMock()
+
+        test_manager.test_definition = td_mock
+
+        test_manager.end()
+
+        td_mock.status.assert_called()
+        td_mock.delete_stacks.assert_called_once()
+
+    @patch("taskcat.testing.manager.Config")
+    def test_end_no_delete(self, mock_config: mm):
+        test_manager = TestManager(mock_config())
+
+        td_mock: mm = MagicMock()
+
+        test_manager.test_definition = td_mock
+
+        test_manager.end(no_delete=True)
+
+        td_mock.delete_stacks.assert_not_called()
+
+    @patch("taskcat.testing.manager.Config")
+    def test_end_keep_failed(self, mock_config: mm):
+        test_manager = TestManager(mock_config())
+
+        td_mock = MagicMock()
+        td_mock.status.return_value = {"COMPLETE": [1], "FAILED": []}
+
+        test_manager.test_definition = td_mock
+
+        test_manager.end(keep_failed=True)
+
+        td_mock.delete_stacks.assert_called_once_with({"status": "CREATE_COMPLETE"})
+
+        td_mock.status.return_value = {"COMPLETE": [1], "FAILED": [1]}
+
+        # Should fail if a stack fails.
+        with self.assertRaises(TaskCatException) as ex:
+            test_manager.end(keep_failed=True)
+
+        self.assertTrue("One or more stacks failed tests:" in str(ex.exception))
+
+    @patch("taskcat.testing.manager.Config")
+    @patch("taskcat.testing.manager._CfnLogTools")
+    @patch("taskcat.testing.manager.ReportBuilder")
+    def test_report(self, mock_report: mm, mock_log: mm, mock_config: mm):
+        test_manager = TestManager(mock_config())
+
+        td_mock = MagicMock()
+
+        test_manager.test_definition = td_mock
+
+        test_manager.report()
+
+        mock_log.return_value.createcfnlogs.assert_called_once()
+        mock_report.return_value.generate_report.assert_called_once()
 
     def test_trim_regions(self):
         pass
