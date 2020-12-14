11736ed994d0a5aec78147451f58667df8e5a583
Levi Blaney <shadycuz@gmail.com>
2020-12-14 04:35:55 -0500

    move test logic from testmanager to deploytest

--- taskcat/testing/manager.py
+++ taskcat/testing/manager.py
@@ -1,266 +1,60 @@
-import logging
-import uuid
-from pathlib import Path
-from typing import Any, Dict, List as ListType, Union
+from typing import List, Union
 
-from taskcat._cfn._log_stack_events import _CfnLogTools
-from taskcat._cfn.threaded import Stacker
-from taskcat._cfn_lint import Lint as TaskCatLint
-from taskcat._cli_core import GLOBAL_ARGS
-from taskcat._client_factory import Boto3Cache
 from taskcat._config import Config
-from taskcat._generate_reports import ReportBuilder
-from taskcat._lambda_build import LambdaBuild
-from taskcat._s3_stage import stage_in_s3
 from taskcat._tui import TerminalPrinter
-from taskcat.exceptions import TaskCatException
+from taskcat.testing import DeployTest
+from taskcat.testing.ab_test import Test
 
-LOG = logging.getLogger(__name__)
+T = List[Test]
 
 
 class TestManager:
+    """Manages the lifecycle of different kinds of Tests. All Tests
+    must implement a run and clean_up method. They must also take a Config and
+    Printer objest. See the Abstract Class Test for more info.
     """
-    Manages the lifecycle of AWS resources while running a test.
-    """
 
-    def __init__(self, config: Config, printer: Union[TerminalPrinter, None] = None):
-        """
-        Creates a TestManager that manages the lifecycle of AWS resources while running
-        a test.
+    def __init__(
+        self,
+        config: Config,
+        printer: Union[TerminalPrinter, None] = None,
+        tests: Union[T, None] = None,
+    ):
 
-        This instance returned has an additonal property test_definiton that is set
-        after the run method is called.
-
-        :param config: takes a taskcat Config object.
-        :param printer: an object capable of reporting test progress
-        """
         self.config = config
-        self.test_definition: Stacker
+        self.printer = printer
+        # The defaults in the future will be Lint, Unit, Deploy in that order.
+        self.tests: T = [DeployTest(config, printer)]
 
-        if printer is None:
-            self.printer = TerminalPrinter(minimalist=True)
-        else:
-            self.printer = printer
+        if tests:
+            self.update_tests(tests)
 
-    @classmethod
-    def from_file(
-        cls,
-        project_root: str = "./",
-        input_file: str = "./.taskcat.yml",
-        regions: str = "ALL",
-        enable_sig_v2: bool = False,
-    ):
-        """
-        Creates a TestManger using a Taskcat config file.
-
-        :param project_root: root path of the project relative to input_file
-        :param input_file: path to a taskcat config file
-        """
-        project_root_path: Path = Path(project_root).expanduser().resolve()
-        input_file_path: Path = project_root_path / input_file
-        # pylint: disable=too-many-arguments
-        args = _build_args(enable_sig_v2, regions, GLOBAL_ARGS.profile)
-        config = Config.create(
-            project_root=project_root_path,
-            project_config_path=input_file_path,
-            args=args
-            # TODO: detect if input file is taskcat config or CloudFormation template
-        )
-
-        return cls(config)
-
-    @classmethod
-    def from_dict(
-        cls,
-        input_config: dict,
-        project_root: str = "./",
-        regions: str = "ALL",
-        enable_sig_v2: bool = False,
-    ):
-        """
-        Creates a TestManger using a Taskcat config file.
-
-        :param project_root: root path of the project relative to input_file
-        :param input_file: path to a taskcat config file
-        """
-        project_root_path: Path = Path(project_root).expanduser().resolve()
-
-        # pylint: disable=too-many-arguments
-        args = _build_args(enable_sig_v2, regions, GLOBAL_ARGS.profile)
-
-        sources = [
-            {"source": "Manual", "config": input_config},
-            {"source": "CliArgument", "config": args},
-        ]
-
-        config = Config(
-            uid=uuid.uuid4(), project_root=project_root_path, sources=sources
-        )
-
-        return cls(config)
-
-    def start(
-        self,
-        test_names: str = "ALL",
-        regions: str = "ALL",
-        skip_upload: bool = False,
-        lint_disable: bool = False,
-    ) -> None:
-        """
-        Starts a Taskcat test run by creating the required resources in AWS.
-
-        :param test_names: comma separated list of tests to run
-        :param regions: comma separated list of regions to test in
-        :param skip_upload: Use templates in an existing cloudformation bucket.
-        :param lint_disable: disable cfn-lint checks
+    def start(self):
+        """Calls the run method of the configured tests.
         """
 
-        _trim_regions(regions, self.config)
-        _trim_tests(test_names, self.config)
+        for test in self.tests:
+            test.run()
 
-        boto3_cache = Boto3Cache()
-
-        templates = self.config.get_templates()
-
-        if skip_upload and not self.config.config.project.s3_bucket:
-            raise TaskCatException(
-                "cannot skip_buckets without specifying s3_bucket in config"
-            )
-
-        buckets = self.config.get_buckets(boto3_cache)
-
-        if not skip_upload:
-            # 1. lint
-            if not lint_disable:
-                lint = TaskCatLint(self.config, templates)
-                errors = lint.lints[1]
-                lint.output_results()
-                if errors or not lint.passed:
-                    raise TaskCatException("Lint failed with errors")
-            # 2. build lambdas
-            if self.config.config.project.package_lambda:
-                LambdaBuild(self.config, self.config.project_root)
-            # 3. s3 sync
-            stage_in_s3(
-                buckets, self.config.config.project.name, self.config.project_root
-            )
-
-        regions = self.config.get_regions(boto3_cache)
-        parameters = self.config.get_rendered_parameters(buckets, regions, templates)
-        tests = self.config.get_tests(templates, regions, buckets, parameters)
-
-        self.test_definition = Stacker(
-            self.config.config.project.name,
-            tests,
-            shorten_stack_name=self.config.config.project.shorten_stack_name,
-        )
-        self.test_definition.create_stacks()
-
-        self.printer.report_test_progress(stacker=self.test_definition)
-
-    def end(
-        self,
-        no_delete: bool = False,
-        keep_failed: bool = False,
-        dont_wait_for_delete: bool = True,
-    ):
-        """
-        Ends a Taskcat test run by deleting the test related resources in AWS.
-
-        :param no_delete: don't delete stacks or buckets
-        :param keep_failed: do not delete failed stacks
-        :param dont_wait_for_delete: returns immediately after calling stack_delete
+    def end(self):
+        """Calls the clean_up method on all configured tests.
         """
 
-        status = self.test_definition.status()
+        for test in self.tests:
+            test.clean_up()
 
-        # Delete Stacks
-        if no_delete:
-            LOG.info("Skipping delete due to cli argument")
-        elif keep_failed:
-            if len(status["COMPLETE"]) > 0:
-                LOG.info("deleting successful stacks")
-                self.test_definition.delete_stacks({"status": "CREATE_COMPLETE"})
-        else:
-            self.test_definition.delete_stacks()
+    def update_tests(self, tests: T) -> None:
+        new_tests: T = []
 
-        if not dont_wait_for_delete:
-            self.printer.report_test_progress(stacker=self.test_definition)
-
-        # TODO: summarise stack statusses (did they complete/delete ok) and print any
-        #  error events
-
-        # Delete Templates and Buckets
-        buckets = self.config.get_buckets()
-
-        if not no_delete or (keep_failed is True and len(status["FAILED"]) == 0):
-            deleted: ListType[str] = []
-            for test in buckets.values():
-                for bucket in test.values():
-                    if (bucket.name not in deleted) and not bucket.regional_buckets:
-                        bucket.delete(delete_objects=True)
-                        deleted.append(bucket.name)
-
-        # 9. raise if something failed
-        # - grabbing the status again to ensure everything deleted OK.
-
-        status = self.test_definition.status()
-        if len(status["FAILED"]) > 0:
-            raise TaskCatException(
-                f'One or more stacks failed to create: {status["FAILED"]}'
-            )
-
-    def report(
-        self, output_directory: str = "./taskcat_outputs",
-    ):
-        """
-        Generates a report of the status of Cloudformation stacks.
-
-        :param output_directory: Where to store generated logfiles
-        """
-        report_path = Path(output_directory).resolve()
-        report_path.mkdir(exist_ok=True)
-        cfn_logs = _CfnLogTools()
-        cfn_logs.createcfnlogs(self.test_definition, report_path)
-        ReportBuilder(
-            self.test_definition, report_path / "index.html"
-        ).generate_report()
-
-
-def _trim_regions(regions, config):
-    if regions != "ALL":
-        for test in config.config.tests.values():
-            to_pop = []
-            idx = 0
-            if test.regions:
-                for _ in test.regions:
-                    if test.regions[idx] not in regions.split(","):
-                        to_pop.append(idx)
-                    idx += 1
-                to_pop.reverse()
-                for idx in to_pop:
-                    test.regions.pop(idx)
-
-
-def _trim_tests(test_names, config):
-    if test_names != "ALL":
-        for test in list(config.config.tests):
-            if test not in test_names.split(","):
-                del config.config.tests[test]
-
-
-def _build_args(enable_sig_v2, regions, default_profile):
-    args: Dict[str, Any] = {}
-    if enable_sig_v2:
-        args["project"] = {"s3_enable_sig_v2": enable_sig_v2}
-    if regions != "ALL":
-        if "project" not in args:
-            args["project"] = {}
-        args["project"]["regions"] = regions.split(",")
-    if default_profile:
-        _auth_dict = {"default": default_profile}
-        if not args.get("project"):
-            args["project"] = {"auth": _auth_dict}
-        else:
-            args["project"]["auth"] = _auth_dict
-    return args
+        # This may have not been the best way to do this
+        # but its important that the lis of tests maintain their order.
+        for i in range(len(self.tests)):
+            match = False
+            for test in tests:
+                # Mypy unable to understand `type(self.tests[i]) is type(test)`
+                if type(self.tests[i]).__name__ == type(test).__name__:
+                    new_tests.append(test)
+                    match = True
+                    break
+            if not match:
+                new_tests.append(self.tests[i])
--- tests/test_testing.py
+++ tests/test_testing.py
@@ -8,7 +8,7 @@ import yaml
 from mock import ANY, MagicMock, Mock, patch
 from taskcat._tui import TerminalPrinter
 from taskcat.exceptions import TaskCatException
-from taskcat.testing import TestManager
+from taskcat.testing import DeployTest
 
 # Save some typing
 m = Mock
@@ -16,16 +16,16 @@ mm = MagicMock
 
 
 class TestTestManager(unittest.TestCase):
-    @patch("taskcat.testing.manager.Config", autospec=True)
+    @patch("taskcat.testing.deploy_test.Config", autospec=True)
     def test_from_file(self, mock_config: mm):
         base_path = "./" if os.getcwd().endswith("/tests") else "./tests/"
         base_path = Path(base_path + "data/nested-fail").resolve()
         input_path = base_path / ".taskcat.yml"
 
-        test_manager = TestManager.from_file(str(base_path))
+        test_manager = DeployTest.from_file(str(base_path))
 
         self.assertIsInstance(
-            test_manager, TestManager, "Should return an instance of TestManager."
+            test_manager, DeployTest, "Should return an instance of DeployTest."
         )
 
         _, kwargs = mock_config.create.call_args_list[0]
@@ -52,19 +52,21 @@ class TestTestManager(unittest.TestCase):
             "Should turn our str into a Path.",
         )
 
-    @patch("taskcat.testing.manager.Config", autospec=True)
+    @patch("taskcat.testing.deploy_test.Config", autospec=True)
     def test_from_dict(self, mock_config: mm):
         base_path = "./" if os.getcwd().endswith("/tests") else "./tests/"
         base_path = Path(base_path + "data/nested-fail").resolve()
         input_path = base_path / ".taskcat.yml"
 
+        mock_config.return_value.uid = "test"
+
         with open(str(input_path)) as f:
             test_config = yaml.load(f, Loader=yaml.FullLoader)
 
-        test_manager = TestManager.from_dict(test_config, project_root=str(base_path))
+        test_manager = DeployTest.from_dict(test_config, project_root=str(base_path))
 
         self.assertIsInstance(
-            test_manager, TestManager, "Should return an instance of TestManager."
+            test_manager, DeployTest, "Should return an instance of DeployTest."
         )
 
         _, kwargs = mock_config.call_args_list[0]
@@ -86,13 +88,17 @@ class TestTestManager(unittest.TestCase):
             self.assertTrue("config" in item, "Should contain config key.")
             self.assertTrue("source" in item, "Should contain source key.")
 
-    @patch("taskcat.testing.manager.Config", autospec=True)
+    @patch("taskcat.testing.deploy_test.Config")
     def test_default(self, mock_config: mm):
 
-        test_manager = TestManager(mock_config)
+        mock_config.return_value.uid = "test"
+
+        test_manager = DeployTest(mock_config())
 
         self.assertEqual(
-            test_manager.config, mock_config, "Should set our config property."
+            test_manager.config,
+            mock_config.return_value,
+            "Should set our config property.",
         )
         self.assertFalse(
             hasattr(test_manager, "test_definition"), "Should not set test_definition."
@@ -103,23 +109,24 @@ class TestTestManager(unittest.TestCase):
             "Should create a TerminalPrinter if none passed in.",
         )
 
-    @patch("taskcat.testing.manager.Config", autospec=True)
+    @patch("taskcat.testing.deploy_test.Config")
     def test_printer_overide(self, mock_config: mm):
+        mock_config.return_value.uid = "test"
 
         mock_printer = MagicMock()
 
-        test_manager = TestManager(mock_config, mock_printer)
+        test_manager = DeployTest(mock_config(), mock_printer)
 
         self.assertEqual(test_manager.printer, mock_printer, "Should use our printer.")
 
-    @patch("taskcat.testing.manager.Stacker", autospec=True)
-    @patch("taskcat.testing.manager.stage_in_s3", autospec=True)
-    @patch("taskcat.testing.manager.LambdaBuild", autospec=True)
+    @patch("taskcat.testing.deploy_test.Stacker", autospec=True)
+    @patch("taskcat.testing.deploy_test.stage_in_s3", autospec=True)
+    @patch("taskcat.testing.deploy_test.LambdaBuild", autospec=True)
     def test_start_default(self, mock_lambda: mm, mock_stage_s3: mm, mock_stacker: mm):
         base_path = "./" if os.getcwd().endswith("/tests") else "./tests/"
         base_path = Path(base_path + "data/nested-fail").resolve()
 
-        test_manager = TestManager.from_file(str(base_path))
+        test_manager = DeployTest.from_file(str(base_path))
 
         # Create all the config mocks
         mock_get_buckets: m = Mock()
@@ -135,7 +142,7 @@ class TestTestManager(unittest.TestCase):
         test_manager.config.get_tests = mock_get_tests
         test_manager.printer = mock_printer
 
-        test_manager.start()
+        test_manager.run()
 
         # Test all the mocks
         mock_get_buckets.assert_called_once()
@@ -165,14 +172,14 @@ class TestTestManager(unittest.TestCase):
             stacker=test_manager.test_definition
         )
 
-    @patch("taskcat.testing.manager.Stacker", autospec=True)
-    @patch("taskcat.testing.manager.stage_in_s3", autospec=True)
-    @patch("taskcat.testing.manager.Config")
+    @patch("taskcat.testing.deploy_test.Stacker", autospec=True)
+    @patch("taskcat.testing.deploy_test.stage_in_s3", autospec=True)
+    @patch("taskcat.testing.deploy_test.Config")
     def test_start_skip_upload(
         self, mock_config: mm, mock_stage_s3: mm, mock_stacker: mm
     ):
 
-        test_manager = TestManager(mock_config())
+        test_manager = DeployTest(mock_config())
 
         config_obj = mock_config.return_value
 
@@ -180,7 +187,7 @@ class TestTestManager(unittest.TestCase):
 
         # Should fail if we don't specify an existing s3 bucket
         with self.assertRaises(TaskCatException) as ex:
-            test_manager.start(skip_upload=True)
+            test_manager.run(skip_upload=True)
 
         self.assertEqual(
             str(ex.exception),
@@ -189,20 +196,20 @@ class TestTestManager(unittest.TestCase):
 
         test_manager.config.config.project.s3_bucket = "FakeBucket"
 
-        test_manager.start(skip_upload=True)
+        test_manager.run(skip_upload=True)
 
         # Test all the mocks
         self.assertFalse(mock_stage_s3.called, "Should not stage in s3.")
 
-    @patch("taskcat.testing.manager.Stacker", autospec=True)
-    @patch("taskcat.testing.manager.stage_in_s3", autospec=True)
-    @patch("taskcat.testing.manager.Config")
-    @patch("taskcat.testing.manager.TaskCatLint", autospec=True)
+    @patch("taskcat.testing.deploy_test.Stacker", autospec=True)
+    @patch("taskcat.testing.deploy_test.stage_in_s3", autospec=True)
+    @patch("taskcat.testing.deploy_test.Config")
+    @patch("taskcat.testing.deploy_test.TaskCatLint", autospec=True)
     def test_start_lint(
         self, mock_lint: mm, mock_config: mm, mock_stage_s3: mm, mock_stacker: mm
     ):
 
-        test_manager = TestManager(mock_config())
+        test_manager = DeployTest(mock_config())
 
         config_obj = mock_config.return_value
         config_obj.config.project.s3_bucket = ""
@@ -211,11 +218,11 @@ class TestTestManager(unittest.TestCase):
         lint_obj.lints = [False, False]
         lint_obj.passed = True
 
-        test_manager.start(lint_disable=True)
+        test_manager.run(lint_disable=True)
 
         lint_obj.output_results.assert_not_called()
 
-        test_manager.start()
+        test_manager.run()
 
         lint_obj.output_results.assert_called_once()
 
@@ -223,45 +230,45 @@ class TestTestManager(unittest.TestCase):
 
         # Should fail if we don't specify an existing s3 bucket
         with self.assertRaises(TaskCatException) as ex:
-            test_manager.start()
+            test_manager.run()
 
         self.assertEqual(str(ex.exception), "Lint failed with errors")
 
-    @patch("taskcat.testing.manager.Config")
+    @patch("taskcat.testing.deploy_test.Config")
     def test_end_default(self, mock_config: mm):
-        test_manager = TestManager(mock_config())
+        test_manager = DeployTest(mock_config())
 
         td_mock = MagicMock()
 
         test_manager.test_definition = td_mock
 
-        test_manager.end()
+        test_manager.clean_up()
 
         td_mock.status.assert_called()
         td_mock.delete_stacks.assert_called_once()
 
-    @patch("taskcat.testing.manager.Config")
+    @patch("taskcat.testing.deploy_test.Config")
     def test_end_no_delete(self, mock_config: mm):
-        test_manager = TestManager(mock_config())
+        test_manager = DeployTest(mock_config())
 
         td_mock: mm = MagicMock()
 
         test_manager.test_definition = td_mock
 
-        test_manager.end(no_delete=True)
+        test_manager.clean_up(no_delete=True)
 
         td_mock.delete_stacks.assert_not_called()
 
-    @patch("taskcat.testing.manager.Config")
+    @patch("taskcat.testing.deploy_test.Config")
     def test_end_keep_failed(self, mock_config: mm):
-        test_manager = TestManager(mock_config())
+        test_manager = DeployTest(mock_config())
 
         td_mock = MagicMock()
         td_mock.status.return_value = {"COMPLETE": [1], "FAILED": []}
 
         test_manager.test_definition = td_mock
 
-        test_manager.end(keep_failed=True)
+        test_manager.clean_up(keep_failed=True)
 
         td_mock.delete_stacks.assert_called_once_with({"status": "CREATE_COMPLETE"})
 
@@ -269,15 +276,15 @@ class TestTestManager(unittest.TestCase):
 
         # Should fail if a stack fails.
         with self.assertRaises(TaskCatException) as ex:
-            test_manager.end(keep_failed=True)
+            test_manager.clean_up(keep_failed=True)
 
         self.assertTrue("One or more stacks failed to create:" in str(ex.exception))
 
-    @patch("taskcat.testing.manager.Config")
-    @patch("taskcat.testing.manager._CfnLogTools")
-    @patch("taskcat.testing.manager.ReportBuilder")
+    @patch("taskcat.testing.deploy_test.Config")
+    @patch("taskcat.testing.deploy_test._CfnLogTools")
+    @patch("taskcat.testing.deploy_test.ReportBuilder")
     def test_report(self, mock_report: mm, mock_log: mm, mock_config: mm):
-        test_manager = TestManager(mock_config())
+        test_manager = DeployTest(mock_config())
 
         td_mock = MagicMock()
 
