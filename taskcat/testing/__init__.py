37ad9ecd557f2ae2217264177244bf9ad7ed88aa
Levi Blaney <shadycuz@gmail.com>
2020-12-16 07:29:50 -0500

    removed TestManager

--- taskcat/testing/__init__.py
+++ taskcat/testing/__init__.py
@@ -1,4 +1,3 @@
 from .cfn_test import CFNTest  # noqa: F401
 from .lint_test import LintTest  # noqa: F401
-from .manager import TestManager  # noqa: F401
 from .unit_test import UnitTest  # noqa: F401
