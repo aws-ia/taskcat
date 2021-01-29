18774c62407f0d63f3adda8b9a9c4916b105ecd3
Jay McConnell <jmmccon@amazon.com>
2021-01-29 09:17:15 -0800

    mock boto

--- tests/testing_module/test_hooks.py
+++ tests/testing_module/test_hooks.py
@@ -7,20 +7,25 @@ from taskcat.exceptions import TaskCatException
 from taskcat.testing import CFNTest
 
 
-class TestCFNTest(unittest.TestCase):
-    def setUp(self):
-        self.project_root_path = Path(__file__).parent / "../data/hook_plugin"
-        self.input_file_path = self.project_root_path / ".taskcat.yml"
+def get_config():
+    project_root_path = Path(__file__).parent / "../data/hook_plugin"
+    input_file_path = project_root_path / ".taskcat.yml"
+    config = Config.create(
+        project_root=project_root_path, project_config_path=input_file_path,
+    )
+    config.get_buckets = MagicMock()
+    config.get_regions = MagicMock()
+    config.get_rendered_parameters = MagicMock()
+    config.get_tests = MagicMock()
+    return config
 
+
+class TestHooks(unittest.TestCase):
     @patch("taskcat.testing._cfn_test.Stacker")
-    def test_execute_hooks_fail(self, mock_stacker: MagicMock):
-        mock_stacker.stacks.return_value = None
-        config = Config.create(
-            project_root=self.project_root_path,
-            project_config_path=self.input_file_path,
-        )
-
-        t = CFNTest(config, lint_disable=True, test_names="hook-fail")
+    @patch("taskcat.testing._cfn_test.Boto3Cache", autospec=True)
+    def test_execute_hooks_fail(self, _m_s: MagicMock, _m_bc: MagicMock):
+        config = get_config()
+        t = CFNTest(config, lint_disable=True, skip_upload=True, test_names="hook-fail")
         with self.assertRaises(TaskCatException) as exc:
             t.run()
         self.assertEqual(
@@ -29,24 +34,20 @@ class TestCFNTest(unittest.TestCase):
         )
 
     @patch("taskcat.testing._cfn_test.Stacker")
-    def test_execute_hooks_pass(self, mock_stacker: MagicMock):
-        mock_stacker.stacks.return_value = None
-        config = Config.create(
-            project_root=self.project_root_path,
-            project_config_path=self.input_file_path,
-        )
-        t = CFNTest(config, lint_disable=True, test_names="hook-pass")
+    @patch("taskcat.testing._cfn_test.Boto3Cache")
+    def test_execute_hooks_pass(self, _m_s: MagicMock, _m_bc: MagicMock):
+        config = get_config()
+        t = CFNTest(config, lint_disable=True, skip_upload=True, test_names="hook-pass")
         t.run()
         self.assertEqual(t.passed, True)
 
     @patch("taskcat.testing._cfn_test.Stacker")
-    def test_execute_hooks_nonexistant(self, mock_stacker: MagicMock):
-        mock_stacker.stacks.return_value = None
-        config = Config.create(
-            project_root=self.project_root_path,
-            project_config_path=self.input_file_path,
+    @patch("taskcat.testing._cfn_test.Boto3Cache")
+    def test_execute_hooks_nonexistant(self, _m_s: MagicMock, _m_bc: MagicMock):
+        config = get_config()
+        t = CFNTest(
+            config, lint_disable=True, skip_upload=True, test_names="hook-nonexist"
         )
-        t = CFNTest(config, lint_disable=True, test_names="hook-nonexist")
         with self.assertRaises(TaskCatException) as exc:
             t.run()
         self.assertEqual(str(exc.exception), 'hook "testhookthatdoesntexist" not found')
