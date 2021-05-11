aad0ac69202a4c65e2b673f43c38fcef876c3ff7
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2021-05-11 12:25:39 -0500

    Hard-coding python; adding ap-northeast-3 to regions_to_partitions

--- e2e/tests/test_import.sh
+++ e2e/tests/test_import.sh
@@ -2,7 +2,7 @@
 
 EXIT_CODE=0
 
-${COV_CMD} -m unittest discover test_imported/ >& /tmp/output || EXIT_CODE=$?
+python -m unittest discover test_imported/ >& /tmp/output || EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} -ne 0 ]] ; then
     echo '$ python -m unittest discover test_imported/'
