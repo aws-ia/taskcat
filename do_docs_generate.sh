346180b93c25d4919ddf86abaeb4541423296398
Tony Vattathil <avattathil@gmail.com>
2021-07-09 21:08:53 -0700

    Add automated push to gh_pages

--- do_docs_generate.sh
+++ do_docs_generate.sh
@@ -13,3 +13,6 @@ pdocs as_html taskcat  -o docs/apidocs/
 # Generate taskcat schema docs
 python3 generate_schema.py
 python3 generate_config_docs.py  >docs/schema/taskcat_schema.md
+
+# Push to gh_pages
+portray on_github_pages --overwrite
