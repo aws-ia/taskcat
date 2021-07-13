c1920be26c1b4bacffd6154f464753f1d2df924e
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2021-07-13 00:48:33 -0500

    Schema tweaks to support examples

--- generate_schema.py
+++ generate_schema.py
@@ -4,7 +4,6 @@ import sys
 from taskcat._dataclasses import BaseConfig
 
 if __name__ == "__main__":
-    if sys.version_info[0] == 3 and sys.version_info[1] == 7:
         schema = BaseConfig.json_schema()
         with open("./taskcat/cfg/config_schema.json", "w") as f:
             f.write(
