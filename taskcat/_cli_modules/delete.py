d98c26c347e9e1374fdd9fc41f10e9d291396bf6
Jarrett Andrulis <jandruli@amazon.com>
2021-07-19 13:33:23 -0500

    Delete seems to be working now - will work on polishing and unit tests

--- taskcat/_cli_modules/delete.py
+++ taskcat/_cli_modules/delete.py
@@ -1,9 +1,12 @@
 # pylint: disable=duplicate-code
 import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import boto3
 
 from taskcat._cfn.stack import Stack
 from taskcat._cfn.threaded import Stacker
 from taskcat._client_factory import Boto3Cache
+from taskcat.regions_to_partitions import REGIONS
 
 LOG = logging.getLogger(__name__)
 
@@ -15,22 +18,36 @@ class Delete:
         self,
         package: str,
         aws_profile: str = "default",
-        region="default",
+        region="ALL",
+        no_verify: bool = False,
         _stack_type="package",
     ):
         """
         :param package: installed package to delete, can be an install name or uuid
         :param aws_profile: aws profile to use for deletion
-        :param region: region to delete from, default will use aws cli configured
-        default
+        :param region: region(s) to delete from, by default, will delete all applicable
+        stacks, supply a csv "us-east-1,us-west-1" to override this default
+        :param no_verify: ignore region verification, delete will not error if an invalid
+        region is detected
         """
         LOG.warning("delete is in alpha feature, use with caution")
         boto3_cache = Boto3Cache()
         if region == "default":
-            region = boto3_cache.get_default_region(aws_profile)
-        if isinstance(region, str):
-            region = [region]
-        stacks = Stacker.list_stacks([aws_profile], region)
+            regions = boto3_cache.get_default_region(aws_profile)
+        elif region == "ALL":
+            region_set: set = set()
+            region_set = region_set.union(
+                # pylint: disable=duplicate-code
+                set(
+                    boto3.Session(profile_name=aws_profile).get_available_regions(
+                        "cloudformation"
+                    )
+                )
+            )
+            regions = list(region_set)
+        elif isinstance(region, str):
+            regions = self._validate_regions(region) if not no_verify else region.split(",")
+        stacks = Stacker.list_stacks([aws_profile], regions)
         jobs = []
         for stack in stacks:
             name = stack.get("taskcat-installer", stack["taskcat-project-name"])
@@ -46,9 +63,37 @@ class Delete:
             if _stack_type == job["type"]:
                 if package in [job["name"], job["taskcat_id"], "ALL"]:
                     jobs.append(job)
-        # TODO: concurrency and wait for complete
-        for job in jobs:
-            client = boto3_cache.client(
-                "cloudformation", profile=aws_profile, region=job["region"]
-            )
-            Stack.delete(client=client, stack_id=job["stack_id"])
+        with ThreadPoolExecutor() as executor:
+            stack_futures = {
+                executor.submit(
+                    self._delete_stack,
+                    boto3_cache=boto3_cache,
+                    job=job,
+                    aws_profile=aws_profile,
+                ): [job["name"], job["region"]]
+                for job in jobs
+            }
+
+            for stack_future in as_completed(stack_futures):
+                name_and_region = stack_futures[stack_future]
+                try:
+                    stack_future.result()
+                except Exception:
+                    LOG.error(f"{name_and_region[0]} failed in {name_and_region[1]}")
+                else:
+                    LOG.info(f"{name_and_region[0]} deleted in {name_and_region[1]}")
+
+    def _delete_stack(self, boto3_cache, job, aws_profile):
+        client = boto3_cache.client(
+            "cloudformation", profile=aws_profile, region=job["region"]
+        )
+        Stack.delete(client=client, stack_id=job["stack_id"])
+
+    # Checks if all regions are valid
+    def _validate_regions(self, region_string):
+        regions = region_string.split(",")
+        for region in regions:
+            if region not in REGIONS:
+                LOG.error(f"Bad region detected: {region}")
+                exit(1)
+        return regions
