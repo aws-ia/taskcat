cc8d0586a7fbe8c269e8c8bfb256a938851ad223
Troy Lindsay <tlindsay42@users.noreply.github.com>
2022-03-02 07:10:22 -0600

    Reformat auto-generated schema files to spec

--- do_docs_generate.sh
+++ do_docs_generate.sh
@@ -11,5 +11,8 @@ portray as_html taskcat  -o docs/apidocs/ --overwrite
 python3 generate_schema.py
 generate-schema-doc --config expand_buttons=true taskcat/cfg/config_schema.json docs/schema/taskcat_schema.md
 
+printf "\n\nReformatting schema files to specifications. Ignore the end-of-file-fixer error.\n\n"
+pre-commit run --all-files
+
 # Push to gh_pages
 portray on_github_pages --overwrite
