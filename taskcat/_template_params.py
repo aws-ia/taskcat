35c89145b6c8d0d3cea2105b54154f9aa0623823
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2022-09-08 13:04:14 -0500

    black/isort

--- taskcat/_template_params.py
+++ taskcat/_template_params.py
@@ -2,10 +2,6 @@ import logging
 import random
 import re
 import uuid
-import dulwich
-from dulwich import errors
-from dulwich.repo import Repo
-# from dulwich.errors import NotGitRepository
 from typing import Set
 
 import dulwich
